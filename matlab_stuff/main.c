724e2cf345d65949973f6c2321205d3df5a3ea13
Bianca Teregan <biancateregan@gmail.com>
2022-12-15 14:42:28 +0200

    added implementation for instruction memory

--- matlab_stuff/main.c
+++ matlab_stuff/main.c
@@ -303,7 +303,40 @@ void control_unit_fcn(uint8_t instruction, boolean *next, boolean *br_oth, boole
     }
 }
 
+void instruction_memory_fcn(uint16_t program_counter, uint6_t *instruction_register, uint10_t *arguments)
+{
+    // asta e byte code generat de asamblor; TBD: cum il dam ca input fara sa l hardcodam?
+    static char instructions[] = "010011000000010001010110000000100000010101110000000001101000100000000";
+
+    // program counterul e numarul instructiunii curente...instructiunea e pe 16 biti
+    // deci pozitia din instructions la care se gaseste instr curenta e &instructions + 16 * PC
+    char current_instruction[17];
+    strncpy(current_instruction, instructions + (program_counter * 16), 16);
+    current_instruction[17] = '\0';
+    // printf("%s\n", current_instruction);
+
+    uint16_t current_instruction_u16 = 0;
+    uint32_t multiplier = 1;
+    int i;
+    for (i = 0xF; i >= 0; i--)
+    {
+        current_instruction_u16 += multiplier * (uint16_t)(current_instruction[i] - '0');
+        // printf("%c %d %d\n", current_instruction[i], multiplier, current_instruction_u16);
+        multiplier *= 2;
+    }
+
+    // printf("%d\n", current_instruction_u16);
+    (*arguments).x = current_instruction_u16 & 0x3FF;                   // primii 10 biti LSB
+    (*instruction_register).x = (current_instruction_u16 >> 10) & 0x3F; // primii 6 biti MSB
+    // printf("%x %d %d\n", current_instruction_u16, (*instruction_register).x, (*arguments).x);
+}
+
 int main()
 {
+    /*
+    uint6_t IR;
+    uint10_t AR;
+    instruction_memory_fcn(1, &IR, &AR);
+    */
     return 0;
 }
